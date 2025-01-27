3e40f851e8ae9e88bdaa497a816fbe98ab42f338
abishejk1872 <abisheknewar15@gmail.com>
2025-01-27 17:06:18 +0530

    changes

---
+++ backend/data.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv"
+dotenv.config()
+
+export const API_KEY = process.env.API_KEY
+export const  AUTH_DOMAIN = process.env.AUTH_DOMAIN
+export const  PROJECT_ID = process.env.PROJECT_ID
+export const  STORAGE_BUCKET = process.env.STORAGE_BUCKET
+export const MESSEGING_SENDER_ID= process.env.MESSEGING_SENDER_ID
+export const  APP_ID= process.env.APP_ID
+export const  MEASUREMENT_ID = process.env.MEASUREMENT_ID
+export const MONGODB_URL = process.env.MONGODB_URL
+export const SECRET_KEY = process.env.SECRET_KEY
+
+

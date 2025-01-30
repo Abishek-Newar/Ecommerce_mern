4a6d584965b8ce48eececf0ce132e2f10815d70b
abishejk1872 <abisheknewar15@gmail.com>
2025-01-30 16:28:28 +0530

    update cors

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,9 @@ import productRouter from "./routes/products.js"
 import userRouter from "./routes/user.js"
 import adminRouter from "./routes/admin.js"
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin: "*"
+}))
 app.use(express.json())
 app.use("/api/products",productRouter)
 app.use("/api/user",userRouter)
